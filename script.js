40a7f1a7001a754a5c6aa464b568f8ae9084a21c
CyanNinja <68496747+Cyan-Ninja@users.noreply.github.com>
2020-08-12 18:19:05 +1000

    Did it!

--- script.js
+++ script.js
@@ -18,13 +18,99 @@ function generatePuzzle() {
 	}
 	for (var x = 0; x < puzzleMatrix.length; x++) {
 		for (var y = 0; y < puzzleMatrix[x].length; y++) {
+			puzzleMatrix[x][y] = {x: true, y: true, f: false, m: false};
 		}
 	}
 	console.log(puzzleMatrix);
 	var startingX = Math.floor(Math.random() * puzzleWidth), endingX;
 	console.log("StartingX: " + startingX);
 	var currentX = startingX, currentY = 0;
+	puzzleMatrix[currentX][currentY].f = true;
 	puzzleMatrix[currentX][currentY].m = true;
+	var filledAll = false;
+	while (!filledAll) {
+		// Get All Unfilled Tiles
+		var unfilledItemsPuzzleMatrix = [];
+		for (var x = 0; x < puzzleWidth; x++) {
+			for (var y = 0; y < puzzleWidth; y++) {
+				if (puzzleMatrix[x][y].f == false) {
+					unfilledItemsPuzzleMatrix.push({x: x, y: y});
+				}
+			}
+		}
+		if (unfilledItemsPuzzleMatrix.length == 1) {
+			filledAll = true;
+		}
+		console.log(unfilledItemsPuzzleMatrix);
+		function shuffle(a) {
+			var j, x, i;
+			for (i = a.length - 1; i > 0; i--) {
+				j = Math.floor(Math.random() * (i + 1));
+				x = a[i];
+				a[i] = a[j];
+				a[j] = x;
+			}
+			return a;
+		}
+		// Find A Suitable Tile & Possible Stems For It
+		unfilledItemsPuzzleMatrix = shuffle(unfilledItemsPuzzleMatrix);
+		var unfilledItem;
+		var directions = [];
+		for (var i = 0; i < unfilledItemsPuzzleMatrix.length; i++) {
+			var unfilledItemTemp = unfilledItemsPuzzleMatrix[i];
+			if (unfilledItemTemp.y - 1 >= 0) { // North
+				if (puzzleMatrix[unfilledItemTemp.x][unfilledItemTemp.y - 1].f == true) {
+					unfilledItem = unfilledItemTemp;
+					directions.push("N");
+				}
+			}
+			if (unfilledItemTemp.y + 1 < puzzleHeight) { // South
+				if (puzzleMatrix[unfilledItemTemp.x][unfilledItemTemp.y + 1].f == true) {
+					unfilledItem = unfilledItemTemp;
+					directions.push("S");
+				}
+			}
+			if (unfilledItemTemp.x - 1 >= 0) { // East
+				if (puzzleMatrix[unfilledItemTemp.x - 1][unfilledItemTemp.y].f == true) {
+					unfilledItem = unfilledItemTemp;
+					directions.push("E");
+				}
+			}
+			if (unfilledItemTemp.x + 1 < puzzleWidth) { // West
+				if (puzzleMatrix[unfilledItemTemp.x + 1][unfilledItemTemp.y].f == true) {
+					unfilledItem = unfilledItemTemp;
+					directions.push("W");
+				}
+			}
+			if (unfilledItem != null) {
+				break;
+			}
+		}
+		console.log(unfilledItem);
+		// Choose Direction
+		var direction = directions[Math.floor(Math.random() * directions.length)];
+		console.log("Dir: " + direction + "  Dirs: " + directions);
+		switch (direction) {
+			case "N":
+				puzzleMatrix[unfilledItem.x][unfilledItem.y].f = true;
+				puzzleMatrix[unfilledItem.x][unfilledItem.y].x = false;
+				break;
+			case "S":
+				puzzleMatrix[unfilledItem.x][unfilledItem.y].f = true;
+				puzzleMatrix[unfilledItem.x][unfilledItem.y + 1].x = false;
+				break;
+			case "E":
+				puzzleMatrix[unfilledItem.x][unfilledItem.y].f = true;
+				puzzleMatrix[unfilledItem.x][unfilledItem.y].y = false;
+				break;
+			case "W":
+				puzzleMatrix[unfilledItem.x][unfilledItem.y].f = true;
+				puzzleMatrix[unfilledItem.x + 1][unfilledItem.y].y = false;
+				break;
+		}
+
+		//filledAll = true; // TEMP: Development Catch-All
+		/*
 		// Get Directions
 		var directions = [];
 		if (currentY != 0) {
@@ -74,10 +160,19 @@ function generatePuzzle() {
 		// Actually Do In That Direction
 		switch (direction) {
 			case "N":
+				puzzleMatrix[currentX][currentY].x = false;
+				currentY -= 1;
+				puzzleMatrix[currentX][currentY].m = true;
 				break;
 			case "E":
+				puzzleMatrix[currentX][currentY].y = false;
+				currentX -= 1;
+				puzzleMatrix[currentX][currentY].m = true;
 				break;
 			case "W":
+				puzzleMatrix[currentX + 1][currentY].y = false;
+				currentX += 1;
+				puzzleMatrix[currentX][currentY].m = true;
 				break;
 			case "S":
 				if (currentY == puzzleHeight - 1) {
@@ -85,11 +180,25 @@ function generatePuzzle() {
 					endingX = currentX;
 					break;
 				} else {
+					puzzleMatrix[currentX][currentY + 1].x = false;
+					currentY += 1;
+					puzzleMatrix[currentX][currentY].m = true;
 					break;
 				}
+			default:
+				direction = "S";
+				if (currentY == puzzleHeight - 1) {
+					atBottom = true;
+					endingX = currentX;
 					break;
+				} else {
+					puzzleMatrix[currentX][currentY + 1].x = false;
+					currentY += 1;
+					puzzleMatrix[currentX][currentY].m = true;
 					break;
+				}
 		}
+		*/
 	}
 	canvasDisplay(puzzleMatrix, startingX, endingX);
 }
@@ -132,6 +241,7 @@ function canvasDisplay(puzzleMatrix, startingX, endingX) {
 	var imagePng = c.toDataURL('image/png');
 	document.getElementById("imageDownload").href = imagePng.replace(/^data:image\/[^;]/, 'data:application/octet-stream');
 	// Individual-Box Marking System
+	for (var x = 0; x < puzzleMatrix.length; x++) {
 		for (var y = 0; y < puzzleMatrix[x].length; y++) {
 			if (puzzleMatrix[x][y].m == true) {
 				ctx.beginPath();
@@ -140,7 +250,10 @@ function canvasDisplay(puzzleMatrix, startingX, endingX) {
 				ctx.closePath();
 			}
 		}
+	}
 	// Option To Save As Answered Image
 	var answeredImagePng = c.toDataURL('image/png');
 	document.getElementById("answeredImageDownload").href = answeredImagePng.replace(/^data:image\/[^;]/, 'data:application/octet-stream');
 }
+
+generatePuzzle(); // TEMP: Just For Development Ease
