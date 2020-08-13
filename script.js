2d53a07fba88bf6701126636b9eef9f5c58ec331
CyanNinja <68496747+Cyan-Ninja@users.noreply.github.com>
2020-08-14 09:48:59 +1000

    Did great work!

--- script.js
+++ script.js
@@ -26,7 +26,90 @@ function generatePuzzle() {
 	console.log("StartingX: " + startingX);
 	var currentX = startingX, currentY = 0;
 	puzzleMatrix[currentX][currentY].f = true;
-	//puzzleMatrix[currentX][currentY].m = true;
+	puzzleMatrix[currentX][currentY].m = true;
+	var answerTiles = [];
+	var atBottom = false;
+	while (!atBottom) {
+		// Get Directions
+		var directions = [];
+		if (currentY != 0 && puzzleMatrix[currentX][currentY - 1].f == false) {
+			var partsNumValue = document.getElementById("nParts").value;
+			var partsNum = 1;
+			if (partsNumValue != "") {
+				partsNum = parseInt(partsNumValue);
+				console.log(partsNumValue);
+			}
+			for (var i = 0; i < partsNum; i++) {
+				directions.push("N");
+			}
+		}
+		if (currentX != 0 && puzzleMatrix[currentX - 1][currentY].f == false) {
+			var partsNumValue = document.getElementById("eParts").value;
+			var partsNum = 1;
+			if (partsNumValue != "") {
+				partsNum = parseInt(partsNumValue);
+			}
+			for (var i = 0; i < partsNum; i++) {
+				directions.push("E");
+			}
+		}
+		if (currentX != puzzleWidth - 1 && puzzleMatrix[currentX + 1][currentY].f == false) {
+			var partsNumValue = document.getElementById("wParts").value;
+			var partsNum = 1;
+			if (partsNumValue != "") {
+				partsNum = parseInt(partsNumValue);
+			}
+			for (var i = 0; i < partsNum; i++) {
+				directions.push("W");
+			}
+		}
+		if (true) {
+			var partsNumValue = document.getElementById("sParts").value;
+			var partsNum = 2;
+			if (partsNumValue != "") {
+				partsNum = parseInt(partsNumValue);
+			}
+			for (var i = 0; i < partsNum; i++) {
+				directions.push("S");
+			}
+		}
+		// Get Direction
+		var direction = directions[Math.floor(Math.random() * directions.length)];
+		console.log("Dir: " + direction + "  Dirs: " + directions);
+		// Actually Do In That Direction
+		switch (direction) {
+			case "N":
+				puzzleMatrix[currentX][currentY].f = true;
+				puzzleMatrix[currentX][currentY - 1].m = true;
+				puzzleMatrix[currentX][currentY].x = false;
+				currentY -= 1;
+				break;
+			case "E":
+				puzzleMatrix[currentX][currentY].f = true;
+				puzzleMatrix[currentX - 1][currentY].m = true;
+				puzzleMatrix[currentX][currentY].y = false;
+				currentX -= 1;
+				break;
+			case "W":
+				puzzleMatrix[currentX][currentY].f = true;
+				puzzleMatrix[currentX + 1][currentY].m = true;
+				puzzleMatrix[currentX + 1][currentY].y = false;
+				currentX += 1;
+				break;
+			case "S":
+				if (currentY == puzzleHeight - 1) {
+					atBottom = true;
+					endingX = currentX;
+					break;
+				} else {
+					puzzleMatrix[currentX][currentY].f = true;
+					puzzleMatrix[currentX][currentY + 1].m = true;
+					puzzleMatrix[currentX][currentY + 1].x = false;
+					currentY += 1;
+					break;
+				}
+		}
+	}
 	var filledAll = false;
 	while (!filledAll) {
 		// Get All Unfilled Tiles
@@ -109,7 +192,6 @@ function generatePuzzle() {
 				break;
 		}
 	}
-	endingX = Math.floor(Math.random() * puzzleWidth);
 
 	canvasDisplay(puzzleMatrix, startingX, endingX);
 }
@@ -119,7 +201,7 @@ function canvasDisplay(puzzleMatrix, startingX, endingX) {
 	var ctx = c.getContext("2d");
 	c.width = puzzleWidth * 50;
 	c.height = puzzleHeight * 50;
-	ctx.lineWidth = 2.5;
+	ctx.lineWidth = 5;
 	ctx.strokeStyle = "#000"; // Black Stroke Colour
 	//ctx.fillStyle = "#000"; // Black Fill Colour
 	ctx.fillStyle = "#044"; // My-Cyan Fill Colour
@@ -144,10 +226,10 @@ function canvasDisplay(puzzleMatrix, startingX, endingX) {
 			}
 		}
 	}
-	ctx.rect(1.25, 1.25, c.width - 2.5, c.height - 2.5);
+	ctx.rect(2.5, 2.5, c.width - 5, c.height - 5);
 	ctx.stroke();
-	ctx.clearRect(startingX * 50 + 1.25, 0, 50 - 2.5, 3.75);
-	ctx.clearRect(endingX * 50 + 1.25, c.height - 3.75, 50 - 2.5, 3.75);
+	ctx.clearRect(startingX * 50 + 2.5, 0, 45, 5);
+	ctx.clearRect(endingX * 50 + 2.5, c.height - 5, 45, 5);
 	// Option To Save As Unanswered Image
 	var imagePng = c.toDataURL('image/png');
 	document.getElementById("imageDownload").href = imagePng.replace(/^data:image\/[^;]/, 'data:application/octet-stream');
@@ -156,7 +238,7 @@ function canvasDisplay(puzzleMatrix, startingX, endingX) {
 		for (var y = 0; y < puzzleMatrix[x].length; y++) {
 			if (puzzleMatrix[x][y].m == true) {
 				ctx.beginPath();
-				ctx.fillRect(x * 50 + 12.5, y * 50 + 12.5, 25, 25);
+				ctx.fillRect(x * 50 + 18.75, y * 50 + 18.75, 12.5, 12.5);
 				ctx.stroke();
 				ctx.closePath();
 			}
@@ -167,4 +249,4 @@ function canvasDisplay(puzzleMatrix, startingX, endingX) {
 	document.getElementById("answeredImageDownload").href = answeredImagePng.replace(/^data:image\/[^;]/, 'data:application/octet-stream');
 }
 
-generatePuzzle(); // TEMP: Just For Development Ease
+//generatePuzzle(); // TEMP: Just For Development Ease
